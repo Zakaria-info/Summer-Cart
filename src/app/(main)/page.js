65621f7d50c825a6d79909a00a65988c0ccbea99
Zakaria-info <info.zakariahossain01@gnail.com>
2026-04-30 16:52:20 +0600

    Added Popular product section as card and show the required info to the card

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,24 +1,16 @@
 import HeroSection from "@/components/shared/homepage/HeroSection";
-
-
+import PopularProducts from "@/components/shared/homepage/PopularProducts";
 
 export default function Home() {
   return (
     <div>
       {/*  Hero Section */}
       <HeroSection />
-      
+
       {/* This is where  Next section will go
-      */}
+       */}
       <div className="container mx-auto px-4 md:px-12 py-16">
-        <h2 className="text-3xl font-bold tracking-tighter text-base-content mb-8">
-          Popular Products
-        </h2>
-        
-        {/* Placeholder for Product Grid (Phase 2) */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-          <p className="text-base-content/70">Incoming products...</p>
-        </div>
+        <PopularProducts />
       </div>
     </div>
   );
