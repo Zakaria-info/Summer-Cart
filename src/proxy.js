181360dc0c0562e19b63fbaf9524c8d475132109
Zakaria-info <info.zakariahossain01@gnail.com>
2026-05-02 15:26:47 +0600

    Trying to soling issue after deployment

--- src/proxy.js
+++ src/proxy.js
@@ -1,24 +1,22 @@
 import { NextResponse } from "next/server";
 
 export function proxy(request) {
- 
-  const session = request.cookies.get("better-auth.session_token");
+  // 1. Check for both local and secure production session tokens
+  const session =
+    request.cookies.get("better-auth.session_token") ||
+    request.cookies.get("__Secure-better-auth.session_token");
 
-  
   if (session) {
     return NextResponse.next();
   }
 
-  
   const { pathname } = request.nextUrl;
   const loginUrl = new URL("/login", request.url);
-  
 
   loginUrl.searchParams.set("callbackURL", pathname);
 
   return NextResponse.redirect(loginUrl);
 }
-
 
 export const config = {
   matcher: ["/productDetails/:path*", "/products"],
